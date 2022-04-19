c4be817a52667f5601ff504b9826655311dcc3d2
Maxim333324 <103577667+Maxim333324@users.noreply.github.com>
2022-04-19 11:59:59 +0300

    Add files via upload
    Начальный проект

---
+++ src/PM03Krugl/PM03Krugl/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PM03Krugl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
